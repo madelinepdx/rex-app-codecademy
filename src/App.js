f2cf2cba2049c4a5b107c41845ed0c9efe3b62f5
Maddy <madelinepdx@gmail.com>
2025-01-02 18:57:35 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to Rex</h1>
+      </header>
+      <main>
+        <p>What do you want?:</p>
+        <button>Eat</button>
+        <button>Drink</button>
+        <button>See</button>
+        <button>Do</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
